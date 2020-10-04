85714ee71e55aed8355c9719e55e5d5addafb0ff
IsraelCarvalho <icarvalho@lear.com>
2020-10-04 14:35:32 -0300

    Resolução Exercício 02

--- Questão 02/QuadradoRedimensionavel.java
+++ Questão 02/QuadradoRedimensionavel.java
@@ -1,9 +1,6 @@
 /*
 Implemente uma classe `QuadradoRedimensionavel`, que é _subclasse_ de 
 `Quadrado` e implementa a interface `Redimensionavel`. 
-
-
-
 Essa classe deve _implementar_  um construtor que recebe o valor de 
 `lado` e o transmite para o construtor da __superclasse__.
 */
@@ -14,6 +11,9 @@ public class QuadradoRedimensionavel extends Quadrado implements Redimensionavel
         super(ladoInformado); //super refere-se a classe pai
     }
 
-    //void redimensionar(int percentual);
+
+    void redimensionar(int percentual){ //Como o exercício apresenta uma interface chamada redimensionar, precisa-se implementar esse método na classe
+
+    }
 
 }
